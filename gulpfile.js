33220dc019e3d480789fe23a538c0e8cc651e501
marijohannessen <mari.l.johannessen@gmail.com>
2016-01-08 18:17:45 -0600

    Minified css

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,21 @@ var    gulp = require('gulp'),
      concat = require('gulp-concat'),
      uglify = require('gulp-uglify'),
        sass = require('gulp-sass'),
-browserSync = require('browser-sync').create();
+browserSync = require('browser-sync').create(),
+    cssnano = require('gulp-cssnano');
 
  gulp.task('compileSass', function() {
   return gulp.src("source/social-media-buttons.scss")
       .pipe(sass())
       .pipe(gulp.dest('./source/'))
       .pipe(browserSync.stream());
+});
+
+gulp.task('minifyCss', function() {
+  return gulp.src('source/social-media-buttons.css')
+      .pipe(cssnano())
+      .pipe(concat('social-media-buttons.min.css'))
+      .pipe(gulp.dest('./source/'))
 });
 
 gulp.task('watchFiles', function() {
@@ -24,7 +32,7 @@ gulp.task('watchFiles', function() {
   gulp.watch('source/**/*.scss', ['compileSass']);
 });
 
-gulp.task("build", ['compileSass'], function() {
+gulp.task("build", ['compileSass', 'minifyCss'], function() {
   return gulp.src(["css/application.css"], { base: './' })
 });
 
